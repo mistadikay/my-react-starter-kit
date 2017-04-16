ce235d570961ef68f75eb810c1164ca8ac10c803
Denis Koltsov <iam@mistadikay.com>
2017-04-16 15:50:34 +0200

    :heavy_minus_sign: remove Edge support

--- postcss.config.js
+++ postcss.config.js
@@ -6,8 +6,7 @@ const plugins = [
   require('autoprefixer')({
     browsers: [
       'last 2 Chrome versions',
-      'last 2 Firefox versions',
-      'last 2 Edge versions'
+      'last 2 Firefox versions'
     ]
   }),
   require('stylelint')(),
