1e6f62c922b426df3ace53ec5483bc85103606f0
Denis Koltsov <iam@mistadikay.com>
2017-04-16 21:19:46 +0200

    :wrench: don’t use unnecessary polyfills if targeting modern browsers

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,5 +1,17 @@
 // @flow
 
-import 'babel-polyfill';
-import 'js-polyfills/url';
-import 'whatwg-fetch';
+/*
+  Modern browsers might need only regenerator runtime
+*/
+import 'regenerator-runtime/runtime';
+
+/*
+  Older browsers might need a bunch of other polyfills
+*/
+// import 'babel-polyfill';
+
+// (http://caniuse.com/#feat=url)
+// import 'js-polyfills/url';
+
+// (http://caniuse.com/#feat=fetch)
+// import 'whatwg-fetch';
